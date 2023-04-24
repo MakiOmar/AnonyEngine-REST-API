14f6f30b8b8f29b6b0546e6b18c0ce9d91859baf
unknown <mo7amed.maki@gmail.com>
2023-04-24 10:19:52 +0200

    Manage dependencies

--- dependancies.php
+++ dependancies.php
@@ -18,18 +18,24 @@ $myUpdateChecker->setBranch('master');
  * Display a notification if one of required plugins is not activated/installed
  */
 add_action( 'admin_notices', function() {
-	if (!defined('ANOENGINE')) {
-	    ?>
-	    <div class="notice notice-error is-dismissible">
-	        <p><?php esc_html_e( 'AnonyEngine rest api plugin requires AnonyEngine plugin to be installed/activated. Please install/activate AnonyEngine plugin first.' ); ?></p>
-	    </div>
-	<?php }
+	if ( ! is_plugin_active( 'bdvs-password-reset/bdvs-password-reset.php' ) || ! is_plugin_active( 'jwt-auth/jwt-auth.php' ) ) {
+        ?>
+        <div class="notice notice-error">
+            <p><?php esc_html_e( 'Either Password Reset with Code for WordPress REST API or JWT Auth plugins need to be active for this plugin to work properly.', 'anonyengine-rest-api' ); ?></p>
+        </div>
+        <?php
+    }
 
 	// Check if JWT Auth – WordPress JSON Web Token Authentication is active
+	/*
 	if (!defined('JWT_AUTH_PLUGIN_DIR')) {
 	    ?>
 	    <div class="notice notice-error is-dismissible">
 	        <p><?php _e( 'AnonyEngine rest api plugin requires <a href="https://wordpress.org/plugins/jwt-auth/">JWT Auth</a> plugin to be installed/activated. Please install/activate JWT Auth plugin first.' ); ?></p>
 	    </div>
-	<?php }
+	<?php }*/
+});
+
+add_action( 'admin_notices', function () {
+    
 });
